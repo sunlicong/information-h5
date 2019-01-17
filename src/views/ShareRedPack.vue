421e2f6a1b26792df5e5c693f36627315b2fb51a
rongtongqi <rongtongqi@51kupai.com>
2019-01-17 15:23:24 +0800

    update

--- src/views/ShareRedPack.vue
+++ src/views/ShareRedPack.vue
@@ -32,8 +32,13 @@ export default {
 		 this.$ui.Indicator.open({
 			text: "加载中...",
 			spinnerType: "snake"
-      	});
-		this.getRedEnvelopeShare();
+		  });
+		var link = window.location.href 
+		if(link.indexOf('from=groupmessage')>-1||link.indexOf('from=singlemessage')>-1||link.indexOf('timeline')>-1){
+			this.$router.push({path:"/GetRedPacket",query:{redpackId:this.redpackId}})
+		}else{
+			this.getRedEnvelopeShare();
+		}
 	},
 	methods:{
 		getRedEnvelopeShare(){
@@ -44,20 +49,27 @@ export default {
 				data:{
 					type:1,
 					pathUrlChat:link,
-					pathUrlApplet:'/pages/getRedPacket/getRedPacket?redpackId='+this.redpackId
+					pathUrlApplet:'/pages/getRedPacket/getRedPacket?redpackId='+this.redpackId,
+					redpackId:this.redpackId
 				},
 			}).then((response) => {
 				this.coderUrl=response.data.data.coderUrl;
 				this.isStatus = true;
 				this.$ui.Indicator.close();
 				if(response.data.data.type ==1){
-					var title = this.$store.state.user.nick +'发了一个拼手气红包，由波场区块链生成，公平公正';
+					var title = this.$store.state.user.nick +'发了一个拼手气红.包，由波场区块链生成，公平公正';
 				}else{
-					var title = this.$store.state.user.nick +'发了一个红包';
+					var title = this.$store.state.user.nick +'发了一个红.包';
+				}
+				if(response.data.data.assetType ==1){
+					var desc = response.data.data.description || '恭喜发财，大吉大利！'
+				}else{
+					var desc = response.data.data.description || '恭喜发财，大吉大利！'
 				}
 				this.$weChat.init({
-					title: this.$store.state.user.nick +'发了一个红包，祝你新年快乐！',
-					desc: response.data.data.description || '恭喜发财，大吉大利！',
+					link:window.location.href,
+					title: title,
+					desc: desc,
 					imgUrl:'https://img.16pic.com/00/60/03/16pic_6003354_s.jpg'
 				});
 			}).catch((response) => {

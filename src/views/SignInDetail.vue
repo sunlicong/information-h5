edfce6aec38634dd1ec24c999b112f49cb4b89c1
licong <sunlicong@51kupai.com>
2019-01-25 16:07:12 +0800

    update

--- src/views/SignInDetail.vue
+++ src/views/SignInDetail.vue
@@ -9,12 +9,12 @@
       <div class="title">区块信息</div>
       <div @click="copy()">
         <span class="key">交易ID：</span>
-        <span class="value">0xoa99…ff62d2</span>
+        <span class="value">{{txId}}</span>
         <img src="~@/assets/image/copy.png" class="copy">
       </div>
       <div>
         <span class="key">区块高度：</span>
-        <span class="value">5602661</span>
+        <span class="value">{{refBlock}}</span>
       </div>
     </div>
   </div>
@@ -23,10 +23,18 @@
 export default {
   name: "SignInDetail",
   data() {
-    return {};
+    return {
+      txId: "0xoa99…ff62d2",
+      refBlock: "5602661"
+    };
   },
   mounted() {},
-  methods: {}
+  methods: {
+    copy() {
+      this.$copyText(this.txId);
+      this.$ui.Toast("复制成功");
+    }
+  }
 };
 </script>
 <style lang="less" scoped>
